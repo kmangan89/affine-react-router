9c6e6717eee9136e413df112263328467f9175ac
Katie Mangan <katie.mangan1@gmail.com>
2019-02-25 16:22:38 -0600

    move about to be second on page

--- src/App.js
+++ src/App.js
@@ -16,15 +16,15 @@ class App extends Component {
           <div>
             <ul>
               <li><Link to="/"> Home </Link></li>
+              <li><Link to="/about"> About </Link></li>
               <li><Link to="/plants"> Plants </Link></li>
               <li><Link to="/animals"> Animals </Link></li>
-              <li><Link to="/about"> About </Link></li>
-
+            
             </ul>
             <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
             <Route path="/plants" component={Plants} />
             <Route path="/animals" component={Animals} />
-            <Route path="/about" component={About} />
           </div>
         </Router>
       </div>
